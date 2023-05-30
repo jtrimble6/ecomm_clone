4375d55407cd75a7913edbdab5309d465b3f8102
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2023-05-30 09:23:26 -0500

    updated AppUrl

--- src/api/AppUrl.jsx
+++ src/api/AppUrl.jsx
@@ -1,7 +1,7 @@
 
 
 class AppUrl {
-    static BaseURL = "http://" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/api"
+    static BaseURL = "http://" + process.env.DB_HOST + "/api"
     static VisitorDetails = this.BaseURL + "/getvisitor"
     static PostContact = this.BaseURL + "/postcontact"
     static AllSiteInfo = this.BaseURL + "/allsiteinfo"
