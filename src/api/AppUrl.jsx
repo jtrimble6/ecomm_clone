ca04db102cf3c874bba4e25e223294f68970fb3c
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2023-05-30 09:45:54 -0500

    updated AppUrl

--- src/api/AppUrl.jsx
+++ src/api/AppUrl.jsx
@@ -1,7 +1,7 @@
 
 
 class AppUrl {
-    static BaseURL = "https://" + process.env.DB_HOST + "/api"
+    static BaseURL = "https://ecommerce2.herokuapp.com/api"
     static VisitorDetails = this.BaseURL + "/getvisitor"
     static PostContact = this.BaseURL + "/postcontact"
     static AllSiteInfo = this.BaseURL + "/allsiteinfo"
